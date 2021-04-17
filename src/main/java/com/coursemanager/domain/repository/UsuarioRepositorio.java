8928c7cfcca840b337e4d68bd8ac65605ed993d9
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-17 16:49:41 -0300

    Create UsuarioRepositorio.java

---
+++ src/main/java/com/coursemanager/domain/repository/UsuarioRepositorio.java
@@ -0,0 +1,18 @@
+package com.coursemanager.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coursemanager.domain.model.UsuarioEntidade;
+
+
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<UsuarioEntidade, Long> {
+
+	Optional<UsuarioEntidade> findByNome(String userName);
+	Optional<UsuarioEntidade> findByEmail(String userName);
+
+}
