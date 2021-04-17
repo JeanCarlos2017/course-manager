cf740f71912818305111c2feaa1068b9e32cc3e0
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-17 17:03:09 -0300

    Update UsuarioController.java
    fim crud usuário

--- src/main/java/com/coursemanager/api/controller/UsuarioController.java
+++ src/main/java/com/coursemanager/api/controller/UsuarioController.java
@@ -1,9 +1,27 @@
 package com.coursemanager.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.coursemanager.domain.model.UsuarioEntidade;
+import com.coursemanager.domain.model.UsuarioLogin;
+import com.coursemanager.domain.service.UsuarioService;
+
 
 @RestController 
 @RequestMapping("/usuario")
@@ -13,5 +31,48 @@ public class UsuarioController {
 	@GetMapping
 	public String index() {
 		return "servidor funcionando!";
+	}
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<UsuarioEntidade>> getAll(){
+		return ResponseEntity.ok(usuarioService.getUsuarioRepository().findAll());
+	}
+	
+	@GetMapping("/buscaPorId/{id}")
+	public ResponseEntity<UsuarioEntidade> findById(@PathVariable long id){
+		return ResponseEntity.ok(usuarioService.getById(id));
+	}
+	@PostMapping("/cadastrar")
+	public ResponseEntity<UsuarioEntidade> postUsuario(@Valid @RequestBody UsuarioEntidade usuario) throws Exception{
+		UsuarioEntidade user = usuarioService.cadastraUsuario(usuario);
+		return this.valida(user, HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<UsuarioLogin> logar(@Valid @RequestBody Optional<UsuarioLogin> userParam){
+		return usuarioService.login(userParam).map(resp -> ResponseEntity.ok(resp))
+									.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PutMapping("/atualizar/{id}")
+	public ResponseEntity<UsuarioEntidade> putUsuario(@Valid @RequestBody UsuarioEntidade usuario, 
+			@PathVariable long id) throws Exception{
+		UsuarioEntidade user = usuarioService.alteraUsuario(usuario, id);
+		return this.valida(user, HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable long id){
+		this.usuarioService.deleteUsuario(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	private ResponseEntity<UsuarioEntidade> valida(UsuarioEntidade user, HttpStatus status){
+		if (user == null) return ResponseEntity.badRequest().build();
+		else return ResponseEntity.status(status).body(user);
 	}
 }
