2aeeb567a072fc5c5781037bc356fa26f450482a
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-17 21:17:00 -0300

    Create CursoRepositorio.java

---
+++ src/main/java/com/coursemanager/domain/repository/CursoRepositorio.java
@@ -0,0 +1,11 @@
+package com.coursemanager.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coursemanager.domain.model.CursoEntidade;
+
+public interface CursoRepositorio extends JpaRepository<CursoEntidade, Long> {
+	List<CursoEntidade> findByNomeIgnoreCase(String nome);
+}
