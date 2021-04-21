2e36dd38873e15889a0a63bc884b8b6ff1de491b
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-21 17:39:00 -0300

    Update UsuarioEntidade.java
    usando getter e setter para tudo

--- src/main/java/com/coursemanager/domain/model/UsuarioEntidade.java
+++ src/main/java/com/coursemanager/domain/model/UsuarioEntidade.java
@@ -22,30 +22,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "tb_usuario")
-@NoArgsConstructor
+@Entity @Table(name = "tb_usuario")
+@NoArgsConstructor @Getter @Setter
 public class UsuarioEntidade {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter
-	@Setter
 	private long id_usuario;
 
 	@NotBlank(message = "{email.not.blank}")
 	@Email(message = "{email.not.valid}")
-	@Getter
-	@Setter
 	private String email;
 
 	@NotBlank(message = "{senha.not.blank}")
-	@Getter
-	@Setter
 	private String senha;
 
 	@NotBlank(message = "{name.not.blank}")
-	@Getter
-	@Setter
 	private String nome;
 
 	// Relação Tema-Postagem
