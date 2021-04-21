ca37b6feb08333f2c98a856736631c59bbe8f47e
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-21 12:44:32 -0300

    correções nas validações

--- src/main/java/com/coursemanager/domain/model/CursoEntidade.java
+++ src/main/java/com/coursemanager/domain/model/CursoEntidade.java
@@ -27,25 +27,35 @@ import java.util.Set;
 public class CursoEntidade {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter private long id;
+	
 	@NotBlank(message = "{name.not.blank}")
 	@Getter @Setter private String nome;
+	
 	@NotBlank(message = "{image.not.blank}")
 	@Getter @Setter private String imageUrl;
-	@NotBlank(message = "{price.not.blank}")
+	
+	@NotNull(message = "{price.not.null}")
 	@Getter @Setter private float price;
+	
 	@NotBlank(message = "{code.not.blank}")
 	@Getter @Setter private String code;
-	@NotBlank(message = "{duration.not.blank}")
-	@Getter @Setter private int duration;
-	@NotBlank(message = "{rating.not.blank}")
+	
+	@NotNull(message = "{duration.not.blank}")
+	@Getter @Setter private float duration;
+	
+	@NotNull(message = "{rating.not.null}")
 	@Getter @Setter private short rating;
-    @NotNull @Temporal(TemporalType.TIMESTAMP)
+    
+	@NotNull @Temporal(TemporalType.TIMESTAMP)
     @Getter private Date releaseDate= new java.sql.Date(System.currentTimeMillis());
-    @NotBlank(message = "{description.not.blank}")
+    
+	@NotBlank(message = "{description.not.blank}")
     @Getter @Setter private String description;
-    @NotBlank(message = "{url.not.blank}")
+    
+	@NotBlank(message = "{url.not.blank}")
     @Getter @Setter private String link_de_acesso;
-    @Getter @Setter private boolean concluido= false;
+    
+	@Getter @Setter private boolean concluido= false;
     
     @ManyToMany(mappedBy= "lista_de_cursos", fetch= FetchType.LAZY)
 	@JsonIgnoreProperties("lista_de_cursos")
