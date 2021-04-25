62359a21432ded5a43f5acef92a13f055429480e
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-24 22:42:45 -0300

    Update MatriculaController.java

--- src/main/java/com/coursemanager/api/controller/MatriculaController.java
+++ src/main/java/com/coursemanager/api/controller/MatriculaController.java
@@ -1,6 +1,6 @@
 package com.coursemanager.api.controller;
 
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,24 +11,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.coursemanager.domain.model.CursoEntidade;
+import com.coursemanager.domain.dto.MatriculaDTO;
+import com.coursemanager.domain.dto.UtilsDTO;
 import com.coursemanager.domain.service.MatriculaService;
 
 @RestController 
-@RequestMapping("/usuario/{id_usuario}/matricula")
+@RequestMapping("/usuario/{id_aluno}/matricular")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MatriculaController {
 	@Autowired
 	private MatriculaService matriculaService;
 	
 	@PostMapping("/curso/{id_curso}")
-	public ResponseEntity<CursoEntidade> matriculaAluno(@PathVariable long id_usuario, 
-			@PathVariable long id_curso) throws Exception{
-		return ResponseEntity.ok(this.matriculaService.matriculaAluno(id_usuario, id_curso));
+	public ResponseEntity<MatriculaDTO> matriculaAluno(@PathVariable long id_aluno	, 
+			@PathVariable long id_curso){
+		return ResponseEntity.ok( new MatriculaDTO(matriculaService.matriculaAluno(id_aluno, id_curso)) );
 	}
 	
-	@GetMapping("/cursos/matriculados")
-	public ResponseEntity<Set<CursoEntidade>> cursosMatriculadosPorAluno(@PathVariable long id_usuario){
-		return ResponseEntity.ok(this.matriculaService.cursosMatriculadosPorAluno(id_usuario));
+	@GetMapping("/listar")
+	public ResponseEntity<List<MatriculaDTO>> listaMatricula(){
+		return ResponseEntity.ok( UtilsDTO.matriculaEntidadeToDTO( matriculaService.listaMatricula()) );
 	}
+	
+	
 }
