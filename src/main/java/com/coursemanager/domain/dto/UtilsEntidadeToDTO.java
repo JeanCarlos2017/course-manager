772eab0b64f837f407a6c86a075545a00fe8f834
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-25 10:57:37 -0300

    refactor UtilsEntidadeToDTO

--- src/main/java/com/coursemanager/domain/dto/UtilsEntidadeToDTO.java
+++ src/main/java/com/coursemanager/domain/dto/UtilsEntidadeToDTO.java
@@ -1,16 +1,24 @@
 package com.coursemanager.domain.dto;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.coursemanager.domain.model.MatriculaEntidade;
 
-public class UtilsDTO {
+public class UtilsEntidadeToDTO {
 	public static List<MatriculaDTO> matriculaEntidadeToDTO(List<MatriculaEntidade> matriculaEntidadeList){
 		List<MatriculaDTO> listaMatriculaDTO= new ArrayList<MatriculaDTO>();
 		for(MatriculaEntidade matriculaEntidade: matriculaEntidadeList) {
 			listaMatriculaDTO.add(new MatriculaDTO(matriculaEntidade));
 		}
 		return listaMatriculaDTO;
+	}
+	
+	public static List<CursoDTO> getCursoDTODeMatriculaEntidade(Collection<MatriculaEntidade> cursoEntidadeList){
+		return cursoEntidadeList.stream()
+				.map( (curso) -> new CursoDTO(curso.getCurso()))
+				.collect(Collectors.toList());
 	}
 }
